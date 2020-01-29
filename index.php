8cd2564a3fd39f4d9da95d2b75a61c522c1ebb91
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-01-29 08:52:10 -1000

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>php Youtube Clone</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+
+<body>
+    <div id="pageContainer">
+        <div id="mastHeadContainer">
+            hello
+        </div>
+        <div id="sideNavContainer">
+        
+        </div>
+
+        <div id="mainSectionContainer">
+        
+        </div>
+    </div>
+</body>
+</html>
