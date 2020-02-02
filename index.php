292491ca2914781d11392776036598a9d13d8cfa
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-02-01 15:42:15 -1000

    creating javascript file

--- index.php
+++ index.php
@@ -2,13 +2,21 @@
 <html>
 <head>
     <title>php Youtube Clone</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="assets/js/commonActions.js"></script>
+
 </head>
 
 <body>
     <div id="pageContainer">
         <div id="mastHeadContainer">
-            hello
+            <button class="navShowHide">Click me</button>
         </div>
         <div id="sideNavContainer">
         
