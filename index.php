3713141ae8c48be954bed4694f1915cbc1b5565d
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-02-05 10:32:19 -1000

    moving navbar into seperate file

--- index.php
+++ index.php
@@ -1,56 +1,8 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>php Youtube Clone</title>
+<?php require_once("includes/header.php"); ?>
 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-    <script src="assets/js/commonActions.js"></script>
+Hello everyone
 
-</head>
 
-<body>
-    <div id="pageContainer">
-        <div id="mastHeadContainer">
-            <button class="navShowHide">
-                <img src="assets/images/icons/menu.png">
-            </button>
 
-            <a class="logoContainer" href="index.php">
-                <img src="assets/images/icons/VideoTubeLogo.png" title="logo"  alt="Site logo">
-            </a>
-
-            <div class="searchBarContainer">
-                <form action="search.php" method="GET">
-                    <input type="text" class="searchBar" name="term" placeholder="Search...">
-                    <button class="searchButton">
-                        <img src="assets/images/icons/search.png">
-                    </button>
-                </form>
-            </div>
-
-            <div class="rightIcons">
-                <a href="upload.php">
-                    <img class="upload" src="assets/images/icons/upload.png">  
-                </a>
-                <a href="#">
-                    <img class="upload" src="assets/images/profilePictures/default.png">
-                </a>
-            </div>
-        </div>
-        <div id="sideNavContainer" style="display: none;">
-        
-        </div>
-
-        <div id="mainSectionContainer">
-            <div id="mainContentContainer">
-                Hello everyone
-            </div>
-        </div>
-    </div>
-</body>
-</html>
+<?php require_once("includes/footer.php"); ?>
