f4d85ccf7955fdcd5ca7168d11c459f47ca5802c
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-02-07 16:40:50 -1000

    created categories dropdown

--- includes/classes/VideoDetailsFormProvider.php
+++ includes/classes/VideoDetailsFormProvider.php
@@ -58,10 +58,20 @@ class VideoDetailsFormProvider {
     private function createCategoriesInput(){
         $query = $this->con->prepare("SELECT * FROM categories");
         $query->execute();
+        $html = "
+        <div class='form-group'>
+            <select class='form-control' name='categoryInput'>";
 
         while($row = $query->fetch(PDO::FETCH_ASSOC)){
-            echo $row['name'] . "<br>";
-}
+            $id = $row['id'];
+            $name = $row['name'];            
+            $html .= "<option value='$id'>$name</option>";
+        }
+
+        $html .= "</select>
+                </div>";
+
+        return $html;
     }
 }
 
