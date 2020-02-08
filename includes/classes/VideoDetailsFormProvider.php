b32dea2a12c8442556a60911e53381f8f06203e4
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-02-07 16:30:05 -1000

    class constructor for video details form provider

--- includes/classes/VideoDetailsFormProvider.php
+++ includes/classes/VideoDetailsFormProvider.php
@@ -1,16 +1,24 @@
 <?php
 
 class VideoDetailsFormProvider {
+    private $con;
+
+    public function __construct($con){
+        $this->con = $con;
+    }
+
     public function createUploadForm(){
         $fileInput = $this->createFileInput();
         $titleInput = $this->createTitleInput();
         $descriptionInput = $this->createDescriptionInput();
         $privacyInput = $this->createPrivacyInput();
+        $categoriesInput = $this->createCategoriesInput();
         return "<form action='processing.php' method='POST'>
                     $fileInput
                     $titleInput
                     $descriptionInput
                     $privacyInput
+                    $categoriesInput
                 </form>";
     }
 
@@ -45,6 +53,15 @@ class VideoDetailsFormProvider {
                     </select>
                 </div>
         ";
+    }
+
+    private function createCategoriesInput(){
+        $query = $this->con->prepare("SELECT * FROM categories");
+        $query->execute();
+
+        while($row = $query->fetch(PDO::FETCH_ASSOC)){
+            echo $row['name'] . "<br>";
+}
     }
 }
 
--- upload.php
+++ upload.php
@@ -6,15 +6,8 @@ require_once("includes/classes/VideoDetailsFormProvider.php");
 <div class="column">
 
 <?php 
-$formProvider = new VideoDetailsFormProvider();
+$formProvider = new VideoDetailsFormProvider($con);
 echo $formProvider->createUploadForm();
-
-$query = $con->prepare("SELECT * FROM categories");
-$query->execute();
-
-while($row = $query->fetch(PDO::FETCH_ASSOC)){
-    echo $row['name'] . "<br>";
-}
 
 ?>
 
