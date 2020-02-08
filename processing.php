2f0b020a26cdd18859c4844e88c1d1b2e18d47f4
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-02-08 12:51:16 -1000

    create video processing page

---
+++ processing.php
@@ -0,0 +1,15 @@
+<?php 
+require_once("includes/header.php"); 
+
+if(!isset($_POST['uploadButton'])){
+    echo "No file sent to page.";
+    exit();
+}
+
+// 1) create file upload data
+
+// 2) process video data (upload)
+
+// 3) check if upload was successful
+?>
+
