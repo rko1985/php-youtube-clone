a63aeaa4331b4a9d15c3b6b7242db8958d72c759
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-02-10 07:45:42 -1000

    checking file size of upload

--- includes/classes/VideoProcessor.php
+++ includes/classes/VideoProcessor.php
@@ -3,6 +3,7 @@
 class VideoProcessor {
 
     private $con;
+    private $sizeLimit = 5;
 
     public function __construct($con){
         $this->con = $con;
@@ -13,9 +14,24 @@ class VideoProcessor {
         $videoData = $videoUploadData->videoDataArray;
 
         $tempFilePath = $targetDir . uniqid() . basename($videoData["name"]);
+        //ex creates string uploads/videos/5aa3e9343c9ffdogs_playing.flx
         $tempFilePath = str_replace(" ", "_", $tempFilePath);
+        $isValidData = $this->processData($videoData, $tempFilePath);
 
         echo $tempFilePath;
+    }
+
+    private function processData($videoData, $filePath){
+        $videoType = pathInfo($filePath, PATHINFO_EXTENSION);
+
+        if(!$this->isValidSize($videoData)){
+            echo "File too large. Can't be more than " . $this->sizeLimit . " bytes";
+            return false;
+        }
+    }
+
+    private function isValidSize($data){
+        return $data["size"] <= $this->sizeLimit;
     }
     
 }
