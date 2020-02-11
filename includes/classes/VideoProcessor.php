fcef4cee1a3c09a6335cfd7bd5abfc296e939767
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-02-11 13:57:46 -1000

    converting videos serverside with FFmpeg

--- includes/classes/VideoProcessor.php
+++ includes/classes/VideoProcessor.php
@@ -5,9 +5,11 @@ class VideoProcessor {
     private $con;
     private $sizeLimit = 500000000;
     private $allowedTypes = array("mp4", "flv", "webm", "mkv", "vob", "ogv", "ogg", "avi", "wmv", "mov", "mpeg", "mpg");
+    private $ffmpegPath;
 
     public function __construct($con){
         $this->con = $con;
+        $this->ffmpegPath = realpath("ffmpeg/bin/ffmpeg.exe");
     }
 
     public function upload($videoUploadData){
@@ -29,6 +31,11 @@ class VideoProcessor {
 
             if(!$this->insertVideoData($videoUploadData, $finalFilePath)){
                 echo "Insert query failed.";
+                return false;
+            }
+
+            if(!$this->convertVideoToMp4($tempFilePath, $finalFilePath)){
+                echo "Upload failed";
                 return false;
             }
 
@@ -82,6 +89,23 @@ class VideoProcessor {
         $query->bindParam(":filePath", $filePath);
 
         return $query->execute();
+    }
+
+    public function convertVideoToMp4($tempFilePath, $finalFilePath){
+        $cmd = "$this->ffmpegPath -i $tempFilePath $finalFilePath 2>&1";
+
+        $outputLog = array();
+        exec($cmd, $outputLog, $returnCode);
+
+        if($returnCode != 0){
+            //Command failed
+            foreach($outputLog as $line){
+                echo $line . "<br>";
+            }
+            return false;
+        }
+
+        return true;
     }
     
 }
