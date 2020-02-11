92416f512d7999f518bd0a6d8d34e1e7557f6a2f
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-02-11 12:09:58 -1000

    fixed errors in description/category name and uploading to database table

--- includes/classes/VideoUploadData.php
+++ includes/classes/VideoUploadData.php
@@ -2,14 +2,14 @@
 
 class VideoUploadData {
 
-    public $videoDataArray, $title, $desciption, $privacy, $category, $uploadedBy;
+    public $videoDataArray, $title, $description, $privacy, $category, $uploadedBy;
 
-    public function __construct($videoDataArray, $title, $desciption, $privacy, $category, $uploadedBy){
+    public function __construct($videoDataArray, $title, $description, $privacy, $category, $uploadedBy){
         $this->videoDataArray = $videoDataArray;
         $this->title = $title;
-        $this->desciption = $desciption;
+        $this->description = $description;
         $this->privacy = $privacy;
-        $this->$category = $category;
+        $this->category = $category;
         $this->uploadedBy = $uploadedBy;
     }
     
