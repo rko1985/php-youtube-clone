f731df81c933fd8d4a4877a6b069edec5559e6b7
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-02-12 11:56:15 -1000

    inserting formatted duration into db

--- includes/classes/VideoProcessor.php
+++ includes/classes/VideoProcessor.php
@@ -138,8 +138,6 @@ class VideoProcessor {
         $videoId = $this->con->lastInsertId();
 
         $this->updateDuration($duration, $videoId);
-
-        echo "duration $duration";
     }
 
     private function getVideoDuration($filePath){
@@ -147,9 +145,22 @@ class VideoProcessor {
     }
 
     private function updateDuration($duration, $videoId){
+        $duration = (int)$duration;
+
         $hours = floor($duration / 3600);
         $mins = floor(($duration - ($hours * 3600)) / 60);
         $secs = floor($duration % 60);
+
+        $hours = ($hours < 1) ? "" : $hours . ":";
+        $mins = ($mins < 10) ? "0" . $mins . ":" : $mins . ":";
+        $secs = ($secs < 10) ? "0" . $secs : $secs;
+
+        $duration = $hours . $mins . $secs;
+
+        $query = $this->con->prepare("UPDATE videos SET duration=:duration WHERE id=:videoId");
+        $query->bindparam(":duration", $duration);
+        $query->bindparam(":videoId", $videoId);
+        $query->execute();
     }
     
 }
