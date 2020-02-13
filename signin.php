c2bbdfbc0e5b08345b1e2d606e8d4fe8de8cb8d0
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-02-13 10:57:42 -1000

    sign up page structure created

---
+++ signin.php
@@ -0,0 +1,30 @@
+<?php require_once("includes/config.php"); ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>php Youtube Clone</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
+</head>
+
+<body>
+
+    <div class="signInContainer">
+        <div class="column">
+            <div class="header">
+            
+            </div>
+            <div class="loginForm">
+            </div>
+            <a class="signInMessage" href="signIn.php">Already have an account? Sign in here!</a>
+        </div>
+    </div>
+
+</body>
+</html>
