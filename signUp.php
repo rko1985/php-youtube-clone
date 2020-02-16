9fc6914101e15ae662442c23339bf486fce60a2c
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-02-15 15:29:57 -1000

    styling signup form inputs

--- signUp.php
+++ signUp.php
@@ -28,9 +28,9 @@
                     <input type="text" name="lastName" placeholder="Last Name" autocomplete="off" required>
                     <input type="text" name="username" placeholder="Username" autocomplete="off" required>
                     <input type="email" name="email" placeholder="Email" autocomplete="off" required>
-                    <input type="email2" name="email2" placeholder="Confirm email" autocomplete="off" required>
+                    <input type="email" name="email2" placeholder="Confirm email" autocomplete="off" required>
                     <input type="password" name="password" placeholder="Password" autocomplete="off" required>
-                    <input type="password2" name="password2" placeholder="Confirm password" autocomplete="off" required>
+                    <input type="password" name="password2" placeholder="Confirm password" autocomplete="off" required>
                     <input type="submit" name="submitButton" value="Submit">
                 </form>
             </div>
