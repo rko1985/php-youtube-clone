4358de5442ca880741594eb27880b01adb400374
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-02-17 11:42:21 -1000

    creating account class

---
+++ includes/classes/Account.php
@@ -0,0 +1,16 @@
+<?php 
+
+class Account {
+
+    private $con;
+
+    public function __construct($con){
+        $this->con = $con;
+    }
+
+    public function register($fn, $ln, $un, $em, $em2, $pw, $pw2){
+        
+    }
+}
+
+?>
--- signUp.php
+++ signUp.php
@@ -1,6 +1,9 @@
 <?php 
 require_once("includes/config.php");
 require_once("includes/classes/FormSanitizer.php");
+require_once("includes/classes/Account.php");
+
+$account = new Account($con);
 
 if(isset($_POST['submitButton'])){
     $firstName =  FormSanitizer::sanitizeFormString($_POST['firstName']);
@@ -14,9 +17,7 @@ if(isset($_POST['submitButton'])){
     $password =  FormSanitizer::sanitizeFormPassword($_POST['password']);
     $password2 =  FormSanitizer::sanitizeFormPassword($_POST['password2']);
 
-    echo $firstName . "\n";
-    echo $lastName . "\n";
-    echo $email . "\n";
+    $account->register($firstName, $lastName, $username, $email, $email2, $password, $password2);
 }
 
 ?>
