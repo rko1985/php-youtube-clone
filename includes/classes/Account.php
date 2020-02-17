8f25275bae22eea62156eff3bff200d17de90c33
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-02-17 12:11:19 -1000

    starting register validation function

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -3,13 +3,20 @@
 class Account {
 
     private $con;
+    private $errorArray = array();
 
     public function __construct($con){
         $this->con = $con;
     }
 
     public function register($fn, $ln, $un, $em, $em2, $pw, $pw2){
-        
+        $this->validateFirstName($fn);
+    }
+
+    private function validateFirstName($fn) {
+        if(strlen($fn) > 25 || strlen($fn) < 2){
+            array_push($this->errorArray, Constants::$firstNameCharacters);
+        }
     }
 }
 
