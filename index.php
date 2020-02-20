9855ca4ef4462143173c1915f16f895bca8001d9
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-02-20 13:06:14 -1000

    creating User class functions

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <?php
 
 if(isset($_SESSION['userLoggedIn'])) {
-    echo "user is logged in as " . $_SESSION['userLoggedIn'];
+    echo "user is logged in as " . $userLoggedInObj->getName();
 } else {
     echo "not logged in";
 }
