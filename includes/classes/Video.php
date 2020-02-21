58c02af47219654b691a4ee60e2932a32e1fb0f0
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-02-20 14:07:50 -1000

    creating Video class

---
+++ includes/classes/Video.php
@@ -0,0 +1,53 @@
+<?php 
+
+class Video{
+
+    private $con, $sqlData, $userLoggedInObj;
+
+    public function __construct($con, $input, $userLoggedInObj){
+        $this->con = $con;
+        $this->userLoggedInObj = $userLoggedInObj;
+
+        if(is_array($input)){
+            $this->sqlData = $input;
+        } else {
+            $query = $this->con->prepare("SELECT * FROM videos WHERE id = :id");
+            $query->bindParam(":id", $input);
+            $query->execute();
+
+            $this->sqlData = $query->fetch(PDO::FETCH_ASSOC);
+        }
+
+        
+    }
+
+    public function getUsername(){
+        return $this->sqlData["username"];
+    }
+
+    public function getName(){
+        return $this->sqlData["firstName"] . " " . $this->sqlData["lastName"];
+    }
+
+    public function getFirstName(){
+        return $this->sqlData["firstName"];
+    }
+
+    public function getLastName(){
+        return $this->sqlData["lastName"];
+    }
+
+    public function getEmail(){
+        return $this->sqlData["email"];
+    }
+
+    public function getProfilePic(){
+        return $this->sqlData["profilePic"];
+    }
+
+    public function getSignUpDate(){
+        return $this->sqlData["signUpDate"];
+    }
+}
+
+?>
