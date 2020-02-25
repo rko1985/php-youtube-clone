33a0bbe5eb7e90193bdc4f7d80bd032e645f4eb0
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-02-24 14:26:30 -1000

    dislike functionality

--- includes/classes/VideoInfoControls.php
+++ includes/classes/VideoInfoControls.php
@@ -41,7 +41,9 @@ class VideoInfoControls {
         $class = "dislikeButton";
         $imageSrc = "assets/images/icons/thumb-down.png";
 
-        //Change button img if has been liked already
+        if($this->video->wasDislikedBy()){
+            $imageSrc = "assets/images/icons/thumb-down-active.png";
+        }
 
         return ButtonProvider::createButton($text, $imageSrc, $action, $class);
     }
