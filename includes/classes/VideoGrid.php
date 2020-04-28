9847ce6c6bbd6df8dda68683be90f15951c035ee
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-04-28 07:52:17 -1000

    creating VideGridItem class

--- includes/classes/VideoGrid.php
+++ includes/classes/VideoGrid.php
@@ -13,9 +13,44 @@ class VideoGrid{
 
     public function create($videos, $title, $showFilter){
 
-        return "<div class='$this->gridClass'>
-        
+        if($videos == null){
+            $gridItems = $this->generateItems();
+        }else{
+            $gridItems = $this->generateItemsFromVideos();
+        }
+
+        $header = "";
+
+        if($title != null){
+            $header = $this->createGridHeader($title, $showFilter);
+        }
+
+        return "
+                $header
+                <div class='$this->gridClass'>
+                    $gridItems
                 </div>";
+    }
+
+    public function generateItems(){
+        $query = $this->con->prepare("SELECT * FROM videos ORDER BY RAND() LIMIT 15");
+        $query->execute();
+
+        $elementsHtml = "";
+        while($row = $query->fetch(PDO::FETCH_ASSOC)){
+            $video = new Video($this->con, $row, $this->userLoggedInObj);
+            $item = new VideoGrideItem($video, $this->largeMode);
+            $elementsHtml .= $items->create();
+        }
+        return $elementsHtml;
+    }
+
+    public function generateItemsFromVideos(){
+        
+    }
+
+    public function createGridHeader($title, $showFilter){
+        return "";
     }
 }
 
