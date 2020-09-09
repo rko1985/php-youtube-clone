f5656f35a4c2870e5338f1e3d025f54c996c6cd5
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-09-09 06:27:22 -1000

    create profile page

---
+++ includes/classes/ProfileGenerator.php
@@ -0,0 +1,18 @@
+<?php 
+
+class ProfileGenerator{
+    
+    private $con, $userLoggedInObj, $profileUsername;
+
+    public function __construct($con, $userLoggedInObj, $profileUsername){
+        $this->con = $con;
+        $this->userLoggedInObj = $userLoggedInObj;
+        $this->profileUsername = $profileUsername;
+    }
+
+    public function create(){
+        
+    }
+}
+
+?>
