5e073444bee738f171a27263368f4eff3974359b
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-09-10 07:44:35 -1000

    creating ProfileData class

---
+++ includes/classes/ProfileData.php
@@ -0,0 +1,18 @@
+<?php 
+
+class ProfileData{
+
+    private $con, $profileUserObj;
+
+    public function __construct($con, $profileUsername){
+        $this->con = $con;        
+        $this->profileUserObj = new User($con, $profileUsername);
+    }
+
+    public function getProfileUsername(){
+        return $this->profileUserObj->getUsername();
+    }
+
+}
+
+?>
